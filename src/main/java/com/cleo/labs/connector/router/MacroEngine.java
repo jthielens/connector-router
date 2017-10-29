231f7dee0dd0dfc8e5619d021329869d3097a138
John Thielens <jthielens@cleo.com>
2017-10-29 11:39:15 -0400

    improve performance for simple ${tokens} bypassing JS engine

--- src/main/java/com/cleo/labs/connector/router/MacroEngine.java
+++ src/main/java/com/cleo/labs/connector/router/MacroEngine.java
@@ -1,7 +1,11 @@
 package com.cleo.labs.connector.router;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -15,34 +19,74 @@ public class MacroEngine {
     private static final ScriptEngineManager engine_factory = new ScriptEngineManager();
 
     private ScriptEngine engine;
+    private Date now;
+    private String filename;
+    private EDIMetadata metadata;
 
-    public MacroEngine(EDIMetadata metadata, String filename) {
-        engine  = engine_factory.getEngineByName("JavaScript");
-        try {
-            engine.eval("load('nashorn:mozilla_compat.js');");
-            metadata(metadata);
-            filename(filename);
-        } catch (ScriptException e) {
-            // gulp
+    private enum Token {
+        file(Function.identity(), null),
+        base(FilenameUtils::getBaseName, null),
+        ext((fn) -> FilenameUtils.getExtension(fn).replaceFirst("^(?=[^\\.])","."), null),
+        sender(null, (metadata) -> metadata.sender().id()),
+        receiver(null, (metadata) -> metadata.receiver().id()),
+        groupSender(null, (metadata) -> metadata.groupSender().id()),
+        groupReceiver(null, (metadata) -> metadata.groupReceiver().id()),
+        senderQualifier(null, (metadata) -> metadata.sender().qualifier()),
+        receiverQualifier(null, (metadata) -> metadata.receiver().qualifier()),
+        groupSenderQualifier(null, (metadata) -> metadata.groupSender().qualifier()),
+        groupReceiverQualifier(null, (metadata) -> metadata.groupReceiver().qualifier()),
+        type(null, EDIMetadata::type),
+        icn(null, EDIMetadata::icn);
+
+        private final Function<String, String> filenameFunction;
+        private final Function<EDIMetadata, String> metadataFunction;
+
+        private Token(Function<String,String> filenameFunction,
+                Function<EDIMetadata,String> metadataFunction) {
+            this.filenameFunction = filenameFunction;
+            this.metadataFunction = metadataFunction;
+        }
+
+    };
+
+    private void startEngine() {
+        if (engine == null) {
+            engine  = engine_factory.getEngineByName("JavaScript");
+            try {
+                engine.eval("load('nashorn:mozilla_compat.js');"+
+                    "function date(format) { return new java.text.SimpleDateFormat(format).format(now); }");
+                engine.put("now", now);
+                metadata(metadata);
+                filename(filename);
+            } catch (ScriptException e) {
+                // gulp
+            }
         }
     }
 
+    public MacroEngine(EDIMetadata metadata, String filename) {
+        now = new Date();
+        metadata(metadata);
+        filename(filename);
+    }
+
     public MacroEngine filename(String filename) {
-        String base = FilenameUtils.getBaseName(filename);
-        String ext = FilenameUtils.getExtension(filename).replaceFirst("^(?=[^\\.])", "."); // include leading . unless empty
-        engine.put("file", filename);
-        engine.put("base", base);
-        engine.put("ext", ext);
+        this.filename = filename;
+        if (engine != null) {
+            Stream.of(Token.values())
+                    .filter((t) -> t.filenameFunction != null)
+                    .forEach((t) -> engine.put(t.name(), t.filenameFunction.apply(filename)));
+        }
         return this;
     }
 
     public MacroEngine metadata(EDIMetadata metadata) {
-        engine.put("sender", Strings.nullToEmpty(metadata.sender().id()));
-        engine.put("receiver", Strings.nullToEmpty(metadata.receiver().id()));
-        engine.put("groupSender", Strings.nullToEmpty(metadata.groupSender().id()));
-        engine.put("groupReceiver", Strings.nullToEmpty(metadata.groupReceiver().id()));
-        engine.put("type", Strings.nullToEmpty(metadata.type()));
-        engine.put("icn", Strings.nullToEmpty(metadata.icn()));
+        this.metadata = metadata;
+        if (engine != null) {
+            Stream.of(Token.values())
+                    .filter((t) -> t.metadataFunction != null)
+                    .forEach((t) -> engine.put(t.name(), t.metadataFunction.apply(metadata)));
+        }
         return this;
     }
 
@@ -58,7 +102,37 @@ public class MacroEngine {
         return sb.toString();
     }
 
+    public static final Pattern DATEFUNCTION = Pattern.compile("date\\((?:'([^']*)'|\"([^\"]*)\")\\)");
+
+    public String lookup(String name) {
+        Matcher date = DATEFUNCTION.matcher(name);
+        if (date.matches()) {
+            String format = Strings.nullToEmpty(date.group(1)) +
+                    Strings.nullToEmpty(date.group(2));
+            return new SimpleDateFormat(format).format(now);
+        } else {
+            try {
+                Token token = Token.valueOf(name);
+                if (token.filenameFunction != null) {
+                    return Strings.nullToEmpty(token.filenameFunction.apply(filename));
+                } else if (token.metadataFunction != null) {
+                    return Strings.nullToEmpty(token.metadataFunction.apply(metadata));
+                }
+            } catch (IllegalArgumentException e) {
+                // fall through
+            }
+        }
+        return null; // no such token
+    }
+
     public String expr(String macro) {
+        // first try just looking up a simple value
+        String lookup = lookup(macro);
+        if (lookup != null) {
+            return lookup;
+        }
+        // ok, now run the JS engine, starting it if needed
+        startEngine();
         try {
             Object result = engine.eval(macro);
             return result.toString();
--- src/test/java/com/cleo/labs/connector/router/TestMacroEngine.java
+++ src/test/java/com/cleo/labs/connector/router/TestMacroEngine.java
@@ -2,23 +2,75 @@ package com.cleo.labs.connector.router;
 
 import static org.junit.Assert.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.junit.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class TestMacroEngine {
 
+    private static EDIMetadata metadata() {
+        EDIMetadata mock = mock(EDIMetadata.class);
+        when(mock.sender()).thenReturn(new EDIID("FF","FROM"));
+        when(mock.receiver()).thenReturn(new EDIID("RR","TO"));
+        when(mock.groupSender()).thenReturn(new EDIID("FG","GFROM"));
+        when(mock.groupReceiver()).thenReturn(new EDIID("RG","GTO"));
+        when(mock.type()).thenReturn("MOCK");
+        when(mock.icn()).thenReturn("123456");
+        when(mock.isEmpty()).thenReturn(false);
+        return mock;
+    }
+
     @Test
-    public void test() {
+    public void testFilename() {
+        MacroEngine engine = new MacroEngine(metadata(), "filename.ext");
+        assertEquals("FROM", engine.expr("sender"));
+        assertEquals("final=FROM-.ext.", engine.expand("final=${sender}-${ext}."));
+        assertEquals("final=FROM-.ext.", engine.expand("final=${sender+'-'+ext}."));
+    }
+
+    @Test
+    public void testMetadata() {
+        MacroEngine engine = new MacroEngine(metadata(), "filename.ext");
+        assertEquals("FROM", engine.expand("${sender}"));
+        assertEquals("TO", engine.expand("${receiver}"));
+        assertEquals("GFROM", engine.expand("${groupSender}"));
+        assertEquals("GTO", engine.expand("${groupReceiver}"));
+        assertEquals("FF", engine.expand("${senderQualifier}"));
+        assertEquals("RR", engine.expand("${receiverQualifier}"));
+        assertEquals("FG", engine.expand("${groupSenderQualifier}"));
+        assertEquals("RG", engine.expand("${groupReceiverQualifier}"));
+        assertEquals("MOCK", engine.expand("${type}"));
+        assertEquals("123456", engine.expand("${icn}"));
+    }
+
+    @Test
+    public void testCapture() {
         Pattern p = Pattern.compile("(?<sender>\\S+)\\s+(?<receiver>\\S+)\\s+(?<type>\\S+)\\s+(?<icn>\\S+)");
         Matcher m = p.matcher("FROM TO 214 123456");
         m.matches();
         EDIMetadata metadata = new EDIMetadata(m);
         MacroEngine engine = new MacroEngine(metadata, "filename.ext");
-        assertEquals("4", engine.expr("2+2"));
-        assertEquals("FROM", engine.expr("sender"));
-        assertEquals("final=FROM.ext.", engine.expand("final=${sender}${ext}."));
+        assertEquals("FROM", engine.expand("${sender}"));
+        assertEquals("TO", engine.expand("${receiver}"));
+        assertEquals("214", engine.expand("${type}"));
+        assertEquals("123456", engine.expand("${icn}"));
+    }
+
+    @Test
+    public void testDateLookup() {
+        MacroEngine engine = new MacroEngine(metadata(), "filename.ext");
+        Date now = new Date();
+        String yyyyMMdd = new SimpleDateFormat("yyyyMMdd").format(now);
+        assertEquals(yyyyMMdd, engine.expr("date('yyyyMMdd')"));
+        assertEquals(yyyyMMdd, engine.lookup("date('yyyyMMdd')"));
+        assertEquals(yyyyMMdd, engine.expr("date(\"yyyyMMdd\")"));
+        assertEquals(yyyyMMdd, engine.lookup("date(\"yyyyMMdd\")"));
     }
 
 }
